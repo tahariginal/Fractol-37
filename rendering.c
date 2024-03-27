fe08b63c3d390cb7df584752bbc28ae18ff9838d
tahriginal <koulaltaha7@gmail.com>
2024-03-27 03:25:00 +0000

    norm ok

--- rendering.c
+++ rendering.c
@@ -26,7 +26,7 @@ void	choose_fractal(t_point *c, t_point z, t_fractol *fractal)
 	}
 }
 
-void	paint_fractol(int x, int y, t_fractol *fractal)
+void	painter(int x, int y, t_fractol *fractal)
 {
 	int		i;
 	t_point	z;
@@ -39,31 +39,10 @@ void	paint_fractol(int x, int y, t_fractol *fractal)
 	while (i < fractal->iteration)
 	{
 		//z = z^2 + c
-		z = sum_point(square_point(z), c);
-		if ((z.x * z.x) + (z.y * z.y) > 4)
-		{
-			fractal->color = scale(i, BLACK, WHITE, fractal->iteration);
-			my_mlx_pixel_put(&fractal->img, x, y, fractal->color);
-			return ;
-		}
-		i++;
-	}
-	my_mlx_pixel_put(&fractal->img, x, y, 0x011711);
-}
-void	paint_myFractol(int x, int y, t_fractol *fractal)
-{
-	int		i;
-	t_point	z;
-	t_point	c;
-
-	i = 0;
-	z.x = (scale(x, -2, 2, W) * fractal->zoom) + fractal->shift_x;
-	z.y = (scale(y, 2, -2, H) * fractal->zoom) + fractal->shift_y;
-	choose_fractal(&c, z, fractal);
-	while (i < fractal->iteration)
-	{
-		//z = z^3 + c
-		z = sum_point(cube_point(z), c);
+        if (fractal->name[0] == 'm' || fractal->name[0] == 'j')
+		    z = sum_point(square_point(z), c);
+        else if (fractal->name[0] == 'b')
+		    z = sum_point(cube_point(z), c);
 		if ((z.x * z.x) + (z.y * z.y) > 4)
 		{
 			fractal->color = scale(i, BLACK, WHITE, fractal->iteration);
@@ -86,10 +65,7 @@ void	render_fractal(t_fractol *fractal)
 		x = -1;
 		while (++x < W)
 		{
-			if (fractal->name[0] == 'm' || fractal->name[0] == 'j')
-				paint_fractol(x, y, fractal);
-			else if (fractal->name[0] == 'b')
-				paint_myFractol(x, y, fractal);
+			painter(x, y, fractal);
 		}
 	}
 	mlx_put_image_to_window(fractal->mlx, fractal->win, fractal->img.img, 0, 0);
